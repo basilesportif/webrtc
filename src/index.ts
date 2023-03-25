251e87900b4ecda4220a8eccb2c92c44a3c4df0e
timlucmiptev <info@timlucmiptev.space>
2023-03-25 16:33:15 +0100

    init

--- src/index.ts
+++ src/index.ts
@@ -1,114 +1,65 @@
-import { Peer } from "peerjs";
-
-const r = Math.floor(Math.random() * 10).toString();
-const id = document.location.hash == "" ? "4" + r : "5" + r;
-
-const peer = new Peer({
-  host: "peerjs.uqbar.network",
-  port: 443,
-  secure: true,
-  config: {
-    iceServers: [
+const iceServers = [
   { urls: 'stun:65.21.6.180:3478' },
   {
     urls: 'turn:65.21.6.180:3478',
     username: 'timtime',
     credential: 'blahblah',
   },
-],
+];
+
+const signalingServerUrl = 'wss://peerjs.uqbar.network:443';
+const signaling = new WebSocket(signalingServerUrl);
+
+const peerConnectionConfig = { iceServers };
+const peerConnection = new RTCPeerConnection(peerConnectionConfig);
+
+signaling.addEventListener('message', async (event) => {
+  const data = JSON.parse(event.data);
+
+  if (data.type === 'offer') {
+    await peerConnection.setRemoteDescription(data);
+    const answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer);
+    signaling.send(JSON.stringify(answer));
+  } else if (data.type === 'answer') {
+    await peerConnection.setRemoteDescription(data);
+  } else if (data.type === 'icecandidate') {
+    const candidate = new RTCIceCandidate(data);
+    await peerConnection.addIceCandidate(candidate);
   }
 });
 
-console.log(peer);
+peerConnection.addEventListener('icecandidate', (event) => {
+  const candidate = event.candidate;
 
-document.getElementById("myid").innerHTML = id;
-
-//@ts-ignore
-window.copyId = () => {
-  const id = document.getElementById("myid").innerHTML;
-  navigator.clipboard.writeText(id);
-};
-
-//@ts-ignore
-window.pasteId = () => {
-  navigator.clipboard
-    .readText()
-   .then((clipText) => (document.getElementById("peerid").innerText = clipText));
-}
-
-document.querySelector('form').addEventListener('submit', ev => {
-  ev.preventDefault();
-  const msg = (document.getElementById('outbox') as HTMLInputElement).value;
-  console.log(msg);
-  const peerId = (document.getElementById('peerid') as HTMLInputElement).value;
-  if (peerId === id) {
-    alert('You cannot connect to yourself');
-    return;
+  if (candidate) {
+    signaling.send(
+      JSON.stringify({ type: 'icecandidate', candidate: candidate }),
+    );
   }
-  if (peerId === '') {
-    alert('Please enter a peer ID');
-    return;
-  }
-  const conn = peer.connect(peerId);
-  conn.on('error', err => console.log(err));
-
-  // todo: it's never getting the open message. Need to see what's happening
-  // on the TURN server directly
-  conn.on('open', () => {
-    conn.send(`${(msg)}`);
-    console.log(`I sent: ${(msg)}`);
-  });
-
-  conn.on('error', err => console.log(err));
-  (document.getElementById('outbox') as HTMLInputElement).value = '';
 });
 
-peer.on('connection', conn => {
-  const rtcPeerConnection = conn.peerConnection;
-
-  rtcPeerConnection.addEventListener('icecandidate', event => {
-    const iceCandidate = event.candidate;
-    if (iceCandidate) {
-      console.log('ICE Candidate:', iceCandidate);
-    } else {
-      console.log('All ICE Candidates gathered.');
-    }
-  });
-  conn.on('data', data => {
-    document.getElementById('inbox').innerHTML = (data as string);
-    console.log(data);
-  });
+peerConnection.addEventListener('track', (event) => {
+  const remoteStream = event.streams[0];
+  const remoteVideoElement = (document.getElementById('remote-video') as HTMLVideoElement);
+  remoteVideoElement.srcObject = remoteStream;
 });
 
-/** from tutorial at https://medium.com/@otterlord/learn-peer-js-video-chat-app-bfaa0e976263
- *
-**/
-
-//@ts-ignore
-async function callUser(peerId: string) {
-  // grab the camera and mic
-  const stream = await navigator.mediaDevices.getUserMedia({
-    video: true,
+document.getElementById('start-button').addEventListener('click', async () => {
+  const localStream = await navigator.mediaDevices.getUserMedia({
     audio: true,
+    video: true,
   });
 
-  // switch to the video call and play the camera preview
-  document.getElementById("menu").style.display = "none";
-  document.getElementById("live").style.display = "block";
-  const lv = (document.getElementById("local-video") as HTMLVideoElement);
-  lv.srcObject = stream;
-  lv.play();
-
-  // make the call
-  const call = peer.call(peerId, stream);
-  call.on("stream", (stream) => {
-    const rv = (document.getElementById("remote-video") as HTMLVideoElement);
-    rv.srcObject = stream;
-    rv.play();
+  localStream.getTracks().forEach((track) => {
+    peerConnection.addTrack(track, localStream);
   });
-  call.on("error", (err) => {
-    console.log(err);
-  });
-}
 
-peer.on('error', err => console.log(err));
+  const localVideoElement = (document.getElementById('local-video') as HTMLVideoElement);
+  localVideoElement.srcObject = localStream;
+
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+  console.log(offer)
+  signaling.send(JSON.stringify(offer));
+});
